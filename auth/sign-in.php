fbd72fb5563c3258331c6e3eb875350029fdc146
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-05-30 13:57:00 +0700

    Configuration: font, colors

--- auth/sign-in.php
+++ auth/sign-in.php
@@ -4,25 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign In</title>
+    <title>Sign In | Nasi Kebuli Ajb</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../assets/css/auth.css">
-    <link rel="stylesheet" href="../assets/css/font.css">
-    <link rel="stylesheet" href="../assets/css/tailwindcss/">
+    <link rel="stylesheet" href="../libs/font/font.css">
 </head>
 
 <body>
     <section class="row-sign-in">
         <div class="right-col">
-            <h2>Micro<span>UMKM</span>.</h2>
+            <h2>Nasi Kebuli <span>AJB</span>.</h2>
         </div>
         <div class="left-col">
             <div class="circle-blur-first"></div>
             <div class="circle-blur-second"></div>
             <div class="header-auth">
-                <h2>Welcome to Sign In Micro<span>UMKM</span>.</h2>
+                <h2>Selamat datang di Nasi Kebuli<span>AJB</span>.</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore veritatis qui suscipit iure beatae
                     id
                     quos alias quo vitae architecto.</p>
@@ -41,7 +40,6 @@
                 </div>
                 <button name="signIn" class="btn-sign">Sign In</button>
             </form>
-            <p class="account-link">Your haven't already account? <a href="./sign-up.php">Sign Up</a> now!</p>
         </div>
     </section>
 
@@ -52,10 +50,10 @@
 </html>
 
 
-
+<!-- 
 <?php
 
-include "./connection/config.php";
+include "../connection/config.php";
 
 if (isset($_POST['signIn'])) {
     $email = htmlspecialchars($_POST['email']);
@@ -83,4 +81,4 @@ if (isset($_POST['signIn'])) {
 }
 
 
-?>
+?> -->
